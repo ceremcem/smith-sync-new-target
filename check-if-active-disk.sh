88085657ac6e70a95c18ff54ce3a98f556502ab3
Cerem Cem ASLAN <cem@aktos.io>
2026-05-08 12:27:54 +0300

    added disk corruption prevention if testing in VM

--- check-if-active-disk.sh
+++ check-if-active-disk.sh
@@ -9,5 +9,11 @@ if [[ $actual_root_mntpoint == $root_mnt ]]; then
     echo "This disk seems to be the active one. Use targets/rootfs instead. Exiting."
     exit 1
 fi
+if sudo -u $SUDO_USER VBoxManage list runningvms | grep -q "$test_vm_name"; then
+    echo "This disk seems to be booted in VirtualBox VM."
+    echo "We can't proceed in order to prevent disk corruption."
+    sudo -u $SUDO_USER VBoxManage showvminfo "$test_vm_name" | grep -i state
+    exit 2
+fi
 exit 0
 
